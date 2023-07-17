fd5f64eb64eaa2fc8910828b69f4004eab25d626
yuechenw213 <yuechenw@andrew.cmu.edu>
2023-07-17 16:00:18 -0400

    solve tailwindcss

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import {BiArchive} from "react-icons/bi"
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+    <h1 className="text-5xl">
+    <BiArchive className="inline-block text-red-400 align-top"/>
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
